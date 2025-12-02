664acf90e9ee62011bb57cd7db16f7482306a0c1
ZentoTAG <ZentoTAG@users.noreply.github.com>
2025-12-02 16:52:33 +0500

    добавил гитигнор

---
+++ Sem_1/1_3/1_3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+
+	if (n < 5) {
+		n *= 3;
+	}
+	else if (n > 7) {
+		n += 3;
+	}
+	else {
+		n /= 10;
+	}
+	cout << n << endl;
+
+	return 0;
+}
+
